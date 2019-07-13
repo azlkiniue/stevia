c2cc8db7a6391893645e203f2f1f6d5e1c7360b0
Ahmada Yusril <ahmadayusril@gmail.com>
2019-07-13 16:42:07 +0700

    add index

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var apiRoute = require('./api');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'STEVIA' });
 });
 
 router.use('/test', testRoute);
